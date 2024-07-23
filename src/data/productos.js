dda0ce6c7f8f1aedae8d4db130b228b602367cdf
josue <alejandro@MacBook-Air-de-josue.local>
2024-07-23 14:42:30 -0600

    Inicializacion de proyecto

---
+++ src/data/productos.js
@@ -0,0 +1,44 @@
+export const productos = [
+    {   
+        id: 1,
+        nombre: 'Teclado Mecanico',
+        descripcion: '',
+        precio: 0
+    }, 
+
+    {   
+        id: 2,
+        nombre: 'Samsung TV 55',
+        descripcion: '',
+        precio: 0
+    }, 
+
+    {   
+        id: 3,
+        nombre: 'Audifonos Bluetooh',
+        descripcion: '',
+        precio: 0
+    }, 
+
+    {   
+        id: 4,
+        nombre: 'Memoria Ram 16',
+        descripcion: '',
+        precio: 0
+    }, 
+
+    {   
+        id: 5,
+        nombre: 'Monitor Gamer',
+        descripcion: '',
+        precio: 0
+    },
+
+    {   
+        id: 6,
+        nombre: 'CPU Inteligente',
+        descripcion: '',
+        precio: 0
+    }
+
+]
